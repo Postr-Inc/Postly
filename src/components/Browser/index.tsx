e74c77411b83ada1d7916033c95e9b40e2595624
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-06-10 12:18:27 -0500

    In app browser much more secure

--- src/components/Browser/index.tsx
+++ src/components/Browser/index.tsx
@@ -1,20 +1,56 @@
-import { createSignal } from "solid-js";
- 
-export default function Browser(){
+import { createEffect, createSignal } from "solid-js";
+
+export default function Browser() {
     let [url, setUrl] = createSignal("");
-    let [loading, setLoading] = createSignal(false);
+    let [loading, setLoading] = createSignal(true);
+    let [loaded, setLoaded] = createSignal(false);
     let [error, setError] = createSignal(false);
+    let [showFullUrl, setShowFullUrl] = createSignal(false);
+
+    createEffect(()=>{
+        //@ts-ignore
+        window.open = (url: string) =>{
+           if(document.getElementById("browser-modal")){
+             setUrl(url)
+             var t = setInterval(()=>{
+              if(loading() == false && loaded()){
+                //@ts-ignore
+                document.getElementById("browser-modal").showModal()
+                clearInterval(t)
+              }
+             }, 1000)
+           }
+        }
+    })
     return (
-        <div class="modal">
-            <div class="modal-box">
-                <div class="modal-header">
-                    <h2>Modal Title</h2>
-                    <button class="btn btn-clear" aria-label="Close"></button>
-                </div>
-                <div class="modal-body">
-                    <p>Modal content</p>
+
+        <dialog id="browser-modal" class="modal w-full h-full max-w-5xl max-h-5xl">
+            <div class="bg-white">
+                <div class="mockup-browser   w-screen h-screen">
+                    <div class="mockup-browser-toolbar justify-between gap-5">
+                        <div class="input"
+                        onClick={()=>{
+                            setShowFullUrl(!showFullUrl())
+                        }}
+                        >{url()}</div>
+                        <button class="btn btn-circle btn-ghost" onClick={()=>{
+                            setUrl("")
+                            setLoading(true)
+                            setLoaded(false)
+                            //@ts-ignore
+                            document.getElementById("browser-modal").close()
+                        }}> 
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+                            </svg>
+                        </button>
+                    </div>
+                    <embed onload={()=>{
+                        setLoading(false)
+                        setLoaded(true)
+                    }} src={url()} class="w-full h-full" ></embed>
                 </div>
             </div>
-        </div>
+        </dialog>
     )
 }
