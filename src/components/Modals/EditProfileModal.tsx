8629ddc1236676e30f635db7952bbfbe078df6cb
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-06-16 23:15:30 -0500

    Remember to return empty for users that are not logged in!

--- src/components/Modals/EditProfileModal.tsx
+++ src/components/Modals/EditProfileModal.tsx
@@ -12,6 +12,7 @@ export default function EditProfileModal(
         updateUser: (data: any) => void
     }
 ) {
+    if(!api.authStore.model.id) return <div></div>
     const { theme } = useTheme();
     const [avatar, setAvatar] = createSignal(api.authStore.model.avatar);
     const [avatarFile, setAvatarFile] = createSignal<File>();
