dbc4c6c176bbdbcbd304558effeadd102d976db2
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-01 07:06:21 -0500

    Fixed bug where comments length never changes

--- src/components/PostRelated/Post.tsx
+++ src/components/PostRelated/Post.tsx
@@ -92,12 +92,13 @@ function getFileType(file: string) {
 }
 
 export default function Post(props: Props) {
+  console.log(props.comments?.length)
   let { theme } = useTheme();
   let [likes, setLikes] = createSignal<any[]>(props.likes || []);
   let [bookmarks, setBookmarks] = createSignal(props.bookmarked || [])
   let [comments, setComments] = createSignal<any[]>([]);
   let [commentLength, setCommentLength] = createSignal(
-    props?.comments?.length || 0
+    props?.comments?.length || 0, { equals: false }
   );
   let [views, setViews] = createSignal<any[]>(props.views || []);
 
@@ -359,9 +360,14 @@ export default function Post(props: Props) {
 
 
         <Show when={!props.isComment && !window.location.pathname.includes("/view") || props.isComment && window.location.pathname.includes("/view")}>
-          <CardTitle class="absolute  z-[9999]  right-0">
+          <CardTitle class="absolute   right-0">
             <div class="dropdown dropdown-left    ">
-              <div tabindex="0" role="button" class="btn btn-ghost btn-sm m-1">
+              <div tabindex="0" role="button" class="btn z-[10] btn-ghost btn-sm m-1" onClick={() => {
+                if (!api.authStore.model.username) {
+                  requireSignup()
+                  return;
+                }
+              }}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -435,10 +441,12 @@ export default function Post(props: Props) {
                     </a>
                   </li>
                   <li onClick={() => {
-                    window.dispatchEvent(new CustomEvent("block-user", { detail:  {
-                      user: props.expand.author,
-                      setPosts: props.setPosts
-                    }}))
+                    window.dispatchEvent(new CustomEvent("block-user", {
+                      detail: {
+                        user: props.expand.author,
+                        setPosts: props.setPosts
+                      }
+                    }))
                   }}>
                     <a
 
@@ -508,6 +516,7 @@ export default function Post(props: Props) {
           <Carousel >
             <For each={props.files} fallback={<></>}>
               {(item) => (
+                console.log(item, props.id),
                 <CarouselItem
 
                   onClick={() => {
@@ -619,7 +628,7 @@ export default function Post(props: Props) {
                 d="M12 20.25c4.97 0 9-3.694 9-8.25s-4.03-8.25-9-8.25S3 7.444 3 12c0 2.104.859 4.023 2.273 5.48.432.447.74 1.04.586 1.641a4.483 4.483 0 0 1-.923 1.785A5.969 5.969 0 0 0 6 21c1.282 0 2.47-.402 3.445-1.087.81.22 1.668.337 2.555.337Z"
               />
             </svg>
-            {props.comments?.length}
+            <span class="countdown">  <span style={{ "--value": Math.abs(props.comments ? props.comments.length : props.commentCount || commentLength()) }}></span></span>
 
           </div>
           <Show when={!props.hidden || !props.hidden.includes("repostButton")}>
