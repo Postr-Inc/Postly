4b24662bf91f564935245abd2fd4e7fc0fed7e3a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-16 16:50:08 -0700

    Remove gap

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -86,7 +86,7 @@ export default function Home() {
 
       <Switch>
         <Match when={!loading()}>
-          <div class="flex flex-col    gap-5 ">
+          <div class="flex flex-col     ">
 
 
             <Show when={posts()}>
