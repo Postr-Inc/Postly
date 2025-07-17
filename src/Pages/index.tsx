4d8d8077d7de090cbbbcb34d7c23261f977c4d7a
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-17 14:00:12 -0700

    Update index.tsx

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -94,19 +94,10 @@ export default function Home() {
                 {(item, index) => <div class={joinClass(index() == posts().length - 1 ? "sm:mb-[90px]" : "")}>   <Post {...{ navigate, route, params, isLast: true, ...item, id: item.id }} />  </div>}
               </For>
             </Show>
-            <Show when={posts().length < 1 && feed() === "following"}>
+            
+            <Show when={posts().length < 1 }>
               <div class="text-center text-gray-500 dark:text-gray-400 mt-10">
-                Once you start following people, their posts will appear here!
-              </div>
-            </Show>
-            <Show when={posts().length < 1 && feed() === "trending"}>
-              <div class="text-center text-gray-500 dark:text-gray-400 mt-10">
-                No trending posts available at the moment.
-              </div>
-            </Show>
-            <Show when={posts().length < 1 && feed() === "recommended"}>
-              <div class="text-center text-gray-500 dark:text-gray-400 mt-10">
-                No recommended posts available at the moment.
+                No {feed().includes("topic") ? feed().split("topic-")[1] : feed()} posts available at the moment.
               </div>
             </Show>
           </div>
