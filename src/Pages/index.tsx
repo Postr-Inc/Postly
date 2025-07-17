5710fc6df246c06118bc23a4a8dcf40e7192b4b0
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-16 17:08:52 -0700

    Border to stagnat loading posts

--- src/Pages/index.tsx
+++ src/Pages/index.tsx
@@ -113,7 +113,7 @@ export default function Home() {
         </Match>
         <Match when={loading()}>
           <For each={Array.from({ length: 10 })}>
-            {() => <LoadingIndicator />}
+            {() => <div class="border-b border"><LoadingIndicator /></div>}
           </For>
         </Match>
       </Switch>
