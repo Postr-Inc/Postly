116294157a96b5b783f13a025f080862695a0946
Malik Whitten <65188863+MalikWhitten67@users.noreply.github.com>
2025-07-18 14:36:04 -0700

    Several sizing bug fixes

--- src/Utils/Shared/Page.tsx
+++ src/Utils/Shared/Page.tsx
@@ -18,12 +18,10 @@ import JoinPostlyModal from "../Modals/JoinPostlyModal";
 import BlockUserModal from "@/src/components/Modals/BlockedModal";
 import PostlyPlusModal from "@/src/components/Modals/PostlyPlusModal";
 export default function Page(props: { children: any, params: () => any, route: () => string, navigate: any, id: string, hide?: string[] }) {
-
   const [checkedAuth, setCheckedAuth] = createSignal(false);
+
   onMount(async () => {
     await api.checkAuth();
-
-    // If still not valid, try basic token
     if (!api.authStore.isValid()) {
       try {
         await api.authStore.getBasicAuthToken();
@@ -32,50 +30,87 @@ export default function Page(props: { children: any, params: () => any, route: (
       }
     }
   });
+
+  const hideSidebars = ["/auth/login", "/auth/signup", "/auth/forgot"].includes(props.route());
+
   return <>
+    <div id={props.id} class="relative   bg-base-100 sticky flex justify-center">
 
-
-    <div id={props.id} class={joinClass(
-      "relative flex justify-center w-full sm:max-w-screen-xl",
-      "lg:px-0 lg:px-8"
-    )}>
       <Portal>
         <Alert />
         <RegisterModal />
       </Portal>
 
-      <Show when={!["/auth/login", "/auth/signup", "/auth/forgot"].includes(props.route())}>
-        <div class="hidden xl:block mr-6">
-          <SideBarLeft {...props} />
-        </div>
-      </Show>
+      {/* Centered container with max width */}
+      <div class="relative w-full max-w-screen-xl xl:px-4 2xl:px-4 lg:px-8 flex justify-center">
 
-      <div class="flex flex-col w-full max-w-[40rem]">
-        {props.children}
+        {/* Left Sidebar */}
+        {!hideSidebars && (
+          <div
+            class="hidden lg:block scroll overflow-none fixed top-0   p-4"
+            style={{
+              left: "max(calc(50vw - 660px), 2rem)", // aligned to left edge of centered content
+              width: "280px",
+            }}>
+            <SideBarLeft {...props} />
+          </div>
+        )}
+
+        {/* Main content area */}
+        <main
+          class="w-full  2xl:max-w-[694px] xl:max-w-[600px] sm:max-w-screen mx-auto xl:px-4"
+          style={{
+            minWidth: "20px",
+          }}
+        >
+          {props.children}
+        </main>
+
+        {/* Right Sidebar */}
+        {!hideSidebars && (
+          <div
+            class="hidden xl:block fixed top-0 p-4"
+            style={{
+              right: "max(calc(50vw - 620px), 6rem)", // aligned to right edge of centered content
+              width: "280px",
+            }}
+          >
+            <SideBarRight {...props} />
+          </div>
+        )}
       </div>
 
-      <Show when={!["/auth/login", "/auth/signup", "/auth/forgot"].includes(props.route())}>
-        <div class="hidden xl:block ml-6">
-          <SideBarRight {...props} />
+      {/* Bottom navigation for mobile */}
+      {!props.hide?.includes("bottomNav") && (
+        <div class="lg:hidden fixed bottom-0 w-full z-20">
+          <BottomNav />
         </div>
-      </Show>
-
-      {
-        !props.hide?.includes("bottomNav") && <BottomNav />
-      }
+      )}
     </div>
 
     <Portal>
-
       <Browser />
-
       <CreatePostModal />
       <DeleteAccountModal />
       <EditAccountModal />
       <BlockUserModal />
       <JoinPostlyModal />
       <PostlyPlusModal />
-    </Portal>
-
-  </>
+    </Portal>   <style>
+      {`
+       
+        /* Hide scrollbar for all browsers */
+        ::-webkit-scrollbar {
+          display: none;
+        }
+        * {
+          scrollbar-width: none; /* Firefox */
+          -ms-overflow-style: none;  /* IE 10+ */
+        }
+      `}
+    </style>
+  </>;
 }
+
+
+
